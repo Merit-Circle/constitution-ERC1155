1c95b95a9093505860794add804e6659804dc5ae
federava <federrava@gmail.com>
2023-05-31 12:34:02 +0200

    fix: eoa check on already deployed

--- contracts/NotEOA.sol
+++ contracts/NotEOA.sol
@@ -6,8 +6,16 @@ import "./Constitution.sol";
 contract NotEOA {
 
     constructor(
-        address _constitution
+        address _constitution,
+        bool _call
     ) {
+        Constitution constitution = Constitution(_constitution);
+        if(_call) {
+            constitution.mint();
+        }
+    }
+
+    function mintConstitution(address _constitution) public {
         Constitution constitution = Constitution(_constitution);
         constitution.mint();
     }
